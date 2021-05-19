c5fcb60c44b2fd1ed487ef44c79acf8df0358992
Jeremy Roussel <jeremy.roussel13@gmail.com>
2021-05-19 15:24:39 -0500

    Initial Commit

---
+++ Memory Game/Memory_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_GameApp.swift
+//  Memory Game
+//
+//  Created by Jeremy Roussel on 5/19/21.
+//
+
+import SwiftUI
+
+@main
+struct Memory_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
